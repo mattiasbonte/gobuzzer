4e62ab890de7474873951c8b0ee9b8064af7107d
Mattias Bonte <info@mattiasbonte.dev>
2023-06-19 21:44:38 +0200

    feat(buzzer): add telegram buzzer

---
+++ buzzers/telegram.go
@@ -0,0 +1,28 @@
+package buzzer
+
+import (
+	"net/http"
+	"net/url"
+)
+
+type TelegramMsg struct {
+	ChatId string `json:"chat_id"`
+	Text   string `json:"text"`
+}
+
+// TelegramNotification sends a message to a Telegram chat.
+func TelegramNotification(authToken string, chatID string, message string) error {
+	apiURL := "https://api.telegram.org/bot" + authToken + "/sendMessage"
+
+	values := url.Values{}
+	values.Add("chat_id", chatID)
+	values.Add("text", message)
+
+	resp, err := http.Get(apiURL + "?" + values.Encode())
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
