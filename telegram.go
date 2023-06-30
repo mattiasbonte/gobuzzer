6048aca20f7e50ac626959a11931e119d956f5c8
Mattias B <info@mattiasbonte.dev>
2023-06-30 14:38:31 +0200

    feat(telegram): add optional params

--- telegram.go
+++ telegram.go
@@ -3,21 +3,33 @@ package gobuzzer
 import (
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
-type TelegramMsg struct {
-	ChatId string `json:"chat_id"`
-	Text   string `json:"text"`
+type TelegramNotificationOptions struct {
+	ParseMode             string // "MarkdownV2" | "HTML" | "Markdown"
+	DisableWebPagePreview bool
+	DisableNotification   bool
 }
 
 // TelegramNotification sends a message to a Telegram chat.
-func TelegramNotification(authToken string, chatID string, message string) error {
+// https://core.telegram.org/bots/api#sendmessage
+func TelegramNotification(authToken string, chatId string, message string, opts *TelegramNotificationOptions) error {
 	apiURL := "https://api.telegram.org/bot" + authToken + "/sendMessage"
-
 	values := url.Values{}
-	values.Add("chat_id", chatID)
+	values.Add("chat_id", chatId)
 	values.Add("text", message)
-	values.Add("parse_mode", "MarkdownV2")
+
+	// Optional Params
+	if &opts.ParseMode != nil {
+		values.Add("parse_mode", opts.ParseMode)
+	}
+	if &opts.DisableNotification != nil {
+		values.Add("disable_notification", strconv.FormatBool(opts.DisableNotification))
+	}
+	if &opts.DisableWebPagePreview != nil {
+		values.Add("disable_web_page_preview", strconv.FormatBool(opts.DisableWebPagePreview))
+	}
 
 	resp, err := http.Get(apiURL + "?" + values.Encode())
 	if err != nil {
